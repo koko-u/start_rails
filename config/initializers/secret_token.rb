d4eca8e9b93f5fb04c813790f991deae6eb18fd0
koko_u <kozaki.tsuneaki@gmail.com>
2011-02-05 00:44:36 +0900

    第5章 コントローラとビュー(2)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinagiku::Application.config.secret_token = '216f3e05e1ecb5cdb174486a510c2cff0ea63eb9ab1520e703ae158425838c57eef04e9c686c59bfe937c8f402f624524ef406f99209ae76d126d098958d1b7f'
