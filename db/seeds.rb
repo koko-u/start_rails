d31410fc91b3bda4c4855df2920d5e4d0e282968
koko_u <kozaki.tsuneaki@gmail.com>
2011-02-05 02:36:33 +0900

    第8章 タスクの新規登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# -*- coding: utf-8 -*-
+names = [
+  "切手を買う",
+  "報告書を書く",
+  "家賃を払う",
+  "猫の餌を買う",
+  "燃えないゴミを出す"
+]
+
+description = "これは説明です。" * 20
+
+names.each_with_index do |name, i|
+  Task.create(:name => name, :description => description,
+              :due_date => (i-2).days.from_now, :done => i.zero?
+              )
+end
