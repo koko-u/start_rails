a086070039aa941a8029686bf8c00d0526a582b5
koko_u <kozaki.tsuneaki@gmail.com>
2011-02-05 10:18:49 +0900

    タスクの編集機能と削除機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,4 +15,20 @@ class TasksController < ApplicationController
     @task = Task.create(params[:task])
     redirect_to @task
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(params[:task])
+    redirect_to @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 end
